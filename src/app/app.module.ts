fee913c097c56fc34b496afd01e320a9ca483d45
Arathi Prabhakaran <dwithi252gmail.com>
2023-03-17 13:01:53 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './pages/units/navbar/navbar.component';
+import { FooterComponent } from './pages/units/footer/footer.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomebookingComponent } from './pages/homebooking/homebooking.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SingleComponent } from './pages/single/single.component';
+import { BooknowComponent } from './pages/booknow/booknow.component';
+import { PaymentComponent } from './pages/payment/payment.component';
+import { OurserviceComponent } from './pages/ourservice/ourservice.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContactComponent,
+    AboutComponent,
+    RegisterComponent,
+    HomebookingComponent,
+    LoginComponent,
+    SignupComponent,
+    SingleComponent,
+    BooknowComponent,
+    PaymentComponent,
+    OurserviceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
